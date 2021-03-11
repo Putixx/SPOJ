82aba810e4c8d2225c159b036c0afc638efd146e
spearprojectstudio <58002100+spearprojectstudio@users.noreply.github.com>
2021-03-11 11:07:01 +0100

    Obsługa przywitania

---
+++ ConsoleAppInteraktywnie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleAppInteraktywnie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj!");
+
+            Console.Write("Podaj imię: ");
+            string imie = Console.ReadLine();
+
+            Console.Write("Podaj nazwisko: ");
+            string nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Witaj "+imie+" "+nazwisko+"!");
+
+            string napis = string.Format("Witaj {0} {1}!", imie, nazwisko);
+            Console.WriteLine(napis);
+
+            Console.WriteLine("Witaj {0} {1}!", nazwisko, imie);
+
+            Console.WriteLine($"Witaj {imie} {nazwisko}!");
+
+        }
+    }
+}
